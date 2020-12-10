72bdb2f4bb4ad9ca55b7c44d363f521583682df8
KaderCamara <abdel-kader.camara@ism.edu.sn>
2020-12-10 21:23:39 +0000

    fix: change default view to Login.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-       Parent root = FXMLLoader.load(getClass().getResource("/views/dashbord/Dashbord.fxml"));
+       Parent root = FXMLLoader.load(getClass().getResource("/views/authentication/Login.fxml"));
        
  
         Scene scene = new Scene(root);
