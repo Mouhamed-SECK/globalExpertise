8d9bdaa96d8e3d2ea98f3414f47a5d71bb69d599
KaderCamara <abdel-kader.camara@ism.edu.sn>
2020-12-10 18:38:39 +0000

    feat: implements feature add Category

--- src/dao/CategoryDao.java
+++ src/dao/CategoryDao.java
@@ -5,8 +5,18 @@
  */
 package dao;
 
+import dao.Mysql;
+import dao.IDao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.List;
 import models.Category;
+
 
 /**
  *
@@ -14,15 +24,51 @@ import models.Category;
  */
 public class CategoryDao implements IDao<Category>{
    
+    
+    private final String SQL_SELECT_ALL ="SELECT * FROM `categorie`";
+    private final String SQL_INSERT ="INSERT INTO `categorie` (`categoryId`, `name`) VALUES (?,?)";
+    private final String SELECT_CATEGORY_BY_NAME = "SELECT `name` FROM `categorie` WHERE name=? ";
+    
+    private final Mysql mysql;
+    
+    public CategoryDao() {
+        mysql = new Mysql();
+    }
 
+    
+    
     @Override
-    public Category add(Category obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Category add(Category category) {
+        mysql.getConnection();
+        
+        mysql.initPS(SQL_INSERT);
+        
+        PreparedStatement psmt = mysql.getPstm();
+        try {
+            psmt.setString(1, category.getName());
+            
+            psmt.executeUpdate();
+            ResultSet rs = psmt.getGeneratedKeys();
+            if (rs.next()) {
+                int categoryId = rs.getInt(1);
+                category.setCategoryId(categoryId);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            mysql.closeConnection();
+        }
+
+        return category;
     }
 
     @Override
     public List<Category> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
     }
+    
+    
+    
     
 }
