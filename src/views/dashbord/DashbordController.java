f410610c7c455a53783c52bf1c45b84302b33a02
Mouhamed27-99 <medsoseck@gmail.com>
2020-12-13 08:18:33 +0100

    faat:handle listUsers action and loadView content, remove unuse import

--- src/views/dashbord/DashbordController.java
+++ src/views/dashbord/DashbordController.java
@@ -1,24 +1,17 @@
 package views.dashbord;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.scene.control.Label;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import models.Employes;
-import models.User;
 import utilities.Utilities;
 import views.authentication.LoginController;
 
@@ -26,36 +19,31 @@ public class DashbordController implements Initializable {
 
     @FXML
     private Button btnSignout;
-
-    private Pane pnlUsers;
-
-    private Pane pnlOrders;
-    private Pane pnlProducts;
-    private Pane pnlComptable;
-
     @FXML
     private Button btnOrders;
-    @FXML
     private Button btnUsers;
     @FXML
     private Button btnProducts;
     @FXML
     private Button btnComptable;
     @FXML
-    private Button btnOverview;
-
-    private Utilities utils;
-    @FXML
     private Text fullNameLabel;
     @FXML
     private Text roleLabel;
-
-    private Employes loggedUser;
-
+    @FXML
+     private Button listEmployes;
+    @FXML
+    private Button listCustomer;
     @FXML
     private Text departmentLabel;
     @FXML
-    private Button btnUsers1;
+    private AnchorPane mainContent;
+
+    private Utilities utils;
+    private Employes loggedUser;
+   
+   
+   
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -65,26 +53,20 @@ public class DashbordController implements Initializable {
         fullNameLabel.setText(loggedUser.getFirstname() + " " + loggedUser.getName().toUpperCase());
         departmentLabel.setText("Departement : " + loggedUser.getRole());
         roleLabel.setText("Role : " + loggedUser.getRole());
+        try {
+            utils.loadViewContent(mainContent, "/users/EmployesTableViews");
+        } catch (IOException ex) {
+            Logger.getLogger(DashbordController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
     @FXML
-    public void handleClicks(ActionEvent actionEvent) {
-        if (actionEvent.getSource() == btnUsers) {
-            pnlUsers.toFront();
+    public void handleClicks(ActionEvent actionEvent) throws IOException {
+        if (actionEvent.getSource() == listEmployes) {
+           utils.loadViewContent(mainContent, "/users/EmployesTableViews");
         }
-        if (actionEvent.getSource() == btnProducts) {
-            pnlProducts.setStyle("-fx-background-color : #53639F");
-            pnlProducts.toFront();
-        }
-        if (actionEvent.getSource() == btnComptable) {
-            pnlComptable.setStyle("-fx-background-color : #02030A");
-            pnlComptable.toFront();
-        }
-        if (actionEvent.getSource() == btnOrders) {
-            pnlOrders.setStyle("-fx-background-color : #464F67");
-            pnlOrders.toFront();
-        }
+      
         if (actionEvent.getSource() == btnSignout) {
             utils.changeView(actionEvent, "/authentication/Login");
         }
